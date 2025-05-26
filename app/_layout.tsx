29850dd28f6857efa6a04cf635997dcd009e3efb
tatymediina <tatimedinaiuni@gmail.com>
2025-05-26 08:34:42 -0300

    feat: :zap: Borre una línea que estaba mal escrita

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="(auth)/login.tsx" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)/login" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
